8daa4fafe55a236f6626be551fe5266315a83fc9
Yengilmas <lbek458@gmal.com>
2025-03-29 13:32:53 +0500

    insersion sort some update

--- Main.java
+++ Main.java
@@ -12,18 +12,22 @@ class Main {
             System.out.print(num + " ");
         }
 
-
+        //birinchi for bilan hamma array qismlari aylanib chiqyabdi
         for (int i = 0; i < size - 1; i++) {
+            //i index minIndex sifatida qaralyabdi
             minIndex = i;
-
+            //i+1 indexdan boshlab har bir index ga qarab chiqyabdi minIndexni topish uchun
             for (int j = i + 1; j < size; j++) {
-
+                //          har safar j dan bitta oldingi index ni minIndex bilan solishtiryabdi va agar minIndex dan kichik bo'lsa joyini almashtiryabdi
                 if (nums[j] < nums[minIndex]) {
                     minIndex = j;
                 }
+                //TEMP ga min indexni saqlayabdi
                 temp = nums[minIndex];
+                //min index joyiga yangi o'zgaruvchini beryabdi
                 nums[minIndex] = nums[i];
-                nums[i]=temp;
+                //oxirida min indexga
+                nums[i] = temp;
             }
 
         }
