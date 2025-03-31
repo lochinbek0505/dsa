5ea15695041537dcf1f2f424692bae7602993d7d
Yengilmas <lbek458@gmal.com>
2025-03-31 23:25:10 +0500

    insersion sort some update add license quick sort add

--- QuickSort.java
+++ QuickSort.java
@@ -1,9 +1,57 @@
-public class QuickSort {
+class QuickSort {
+
 
     public static void main(String[] args) {
 
+        int arr[] = {5, 6, 3, 7, 1, 9, 10};
+
+
+        quicsort(arr, 0, arr.length - 1);
+
+        for (int num : arr) {
+
+            System.out.print(num + " ");
+
+        }
 
 
     }
+
+    static void quicsort(int[] arr, int low, int high) {
+
+        if (low < high) {
+
+            int pi = partition(arr, low, high);
+
+            quicsort(arr, low, pi - 1);
+            quicsort(arr, pi + 1, high);
+
+        }
+
+
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+
+            }
+
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
 
 }
